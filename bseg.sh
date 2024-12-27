89ec8c99a0478621e1b51363c94bb7e72316aff1
B.Smart <120326341+BS-Eg@users.noreply.github.com>
2024-12-27 15:14:18 -0500

    Update bseg.sh

--- bseg.sh
+++ bseg.sh
@@ -2,9 +2,12 @@
 apt install git -y
 cd ..
 mkdir -p odoo      
-mkdir -p odoo/151      
-mkdir -p odoo/161
-mkdir -p odoo/npm
+#mkdir -p odoo/151      
+#mkdir -p odoo/161
+#mkdir -p odoo/npm
+
+mkdir -p odoo/181
+mkdir -p odoo/182
 
 
 git clone  https://github.com/BS-Eg/DockerBS
